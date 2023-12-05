62c5dfb5e53e3bb58cd73ff21090bb20cf9d94c5
RegiNajoan <36233179+reginajoan@users.noreply.github.com>
2023-12-05 23:46:40 +0700

    Update Create Entity

--- src/main/java/com/juaracoding/rfspringbootrestapi/model/TugasA.java
+++ src/main/java/com/juaracoding/rfspringbootrestapi/model/TugasA.java
@@ -16,21 +16,120 @@ public class TugasA {
     @Id
     @NotNull
     private String IDTugasA;
-    @Column(name = "Nama")
+    @Column(name = "Nama", length = 40)
+    @NotNull
     private String nama;
-    @Column(name = "Alamat")
+    @Column(name = "Alamat", length = 500)
+    @NotNull
     private String alamat;
     @Column(name = "TanggalLahir")
+    @NotNull
     private Date tanggalLahir;
+    @Column(name = "JenisKelamin")
+    @NotNull
+    private Character jenisKelamin;
     @Column(name = "MasihHidup")
+    @NotNull
     private Boolean masihHidup;
     @Column(name = "CreatedDate")
+    @NotNull
     private LocalDateTime createdDate;
     @Column(name = "CreatedBy")
+    @NotNull
     private Integer createdBy;
     @Column(name = "ModifiedDate")
     private LocalDateTime modifiedDate;
     @Column(name = "ModifiedBy")
     private Integer modifiedBy;
+    @Column(name = "IsDelete")
+    @NotNull
     private Boolean isDelete;
+
+    public String getIDTugasA() {
+        return IDTugasA;
+    }
+
+    public void setIDTugasA(String IDTugasA) {
+        this.IDTugasA = IDTugasA;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public Character getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(Character jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public Boolean getMasihHidup() {
+        return masihHidup;
+    }
+
+    public void setMasihHidup(Boolean masihHidup) {
+        this.masihHidup = masihHidup;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Integer getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Integer modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Boolean getDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(Boolean delete) {
+        isDelete = delete;
+    }
 }
